debf662f8c19218a76a82c649d0bc53eda15f5e8
Marmog85 <moghadasi.64@gmail.com>
2025-04-04 17:17:20 +0200

    labb

---
+++ code_alongs/09_security/create_user.py
@@ -0,0 +1,8 @@
+from connect_postgres import connect_postgres
+from sqlalchemy import text
+from read_sql import read_sql_file
+
+Create_users_sql = read_sql_file("create_user.sql")
+with connect_postgres().connect() as conn:
+
+    conn.commit()
---
+++ code_alongs/Labb/school_tables.sql
@@ -0,0 +1,3 @@
+create SCHEMA IF NOT EXISTS Labb_yrkCo;
+
+SET search_path TO Labb_yrkCo;
